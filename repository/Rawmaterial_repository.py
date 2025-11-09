dbbcd34db232ab64d353c98aa61a98e672cb3b99
taranom <trnmbghry2gmail.com>
2025-11-09 00:35:32 -0800

    update

--- repository/Rawmaterial_repository.py
+++ repository/Rawmaterial_repository.py
@@ -1,5 +1,5 @@
 import sqlite3
-from model.raw_material import Raw_material
+from model.rawmaterial import Raw_material
 
 
 class Raw_material_repository:
@@ -19,9 +19,8 @@ class Raw_material_repository:
     def save(self, raw_material):
         self.connect()
         self.cursor.execute(
-            "insert into raw_materials(name, category, unit, quantity, price, purchase_date, expiry_date, location) values (?,?,?,?,?,?)",
-            [raw_material.first_name, raw_material.last_name, raw_material.role, raw_material.username, raw_material.password,
-             raw_material.salary])
+            "insert into Raw_materials(name, category, unit, quantity, price, purchase_date, expiry_date, location) values (?,?,?,?,?,?,?,?)",
+            [raw_material.name,raw_material.category,raw_material.unit,raw_material.quantity,raw_material.purchase_date,raw_material.expiry_date,raw_material.location])
         raw_material.id = self.cursor.lastrowid
         self.connection.commit()
         self.disconnect()
@@ -30,7 +29,7 @@ class Raw_material_repository:
     def update(self, raw_material):
         self.connect()
         self.cursor.execute(
-            "update raw_materials set first_name=?,last_name=?,role=?,username=?,password=?,salary=?) where id=?",
+            "update Raw_materials set first_name=?,last_name=?,role=?,username=?,password=?,salary=?) where id=?",
             [raw_material.first_name, raw_material.last_name, raw_material.role, raw_material.username, raw_material.password,
              raw_material.salary, raw_material.id])
 
@@ -41,56 +40,56 @@ class Raw_material_repository:
     def delete(self, raw_material):
         self.connect()
         self.cursor.execute(
-            "delete from raw_materials where id=?", [id])
+            "delete from Raw_materials where id=?", [raw_material.id])
         self.connection.commit()
         self.disconnect()
         return raw_material
 
-    def find_by_firstname_and_lastname(self, firstname, lastname):
+    def find_by_name(self,name):
         self.connect()
-        self.cursor.execute("select * from raw_materials where first_name=? and last_name=?", [firstname, lastname])
+        self.cursor.execute("select * from Raw_materials where name=?", [name])
         raw_material_list = [Raw_material(*raw_material) for raw_material in self.cursor.fetchall()]
         self.disconnect()
         return raw_material_list
 
-    def find_by_phone_number(self, number):
+    def find_by_category(self,category):
         self.connect()
-        self.cursor.execute("select * from raw_materials where  phone_number=?", [number])
+        self.cursor.execute("select * from Raw_materials where  category=?", [category])
         raw_material_list = [Raw_material(*raw_material) for raw_material in self.cursor.fetchall()]
         self.disconnect()
         return raw_material_list
 
-    def find_by_order_id(self, id):
+    def find_by_quantity(self, quantity):
         self.connect()
-        self.cursor.execute("select * from raw_materials where  order_id=?", [id])
+        self.cursor.execute("select * from Raw_materials where  quantity=?", [quantity])
         raw_material_list = [Raw_material(*raw_material) for raw_material in self.cursor.fetchall()]
         self.disconnect()
         return raw_material_list
 
     def find_by_role(self, role):
         self.connect()
-        self.cursor.execute("select * from raw_materials where  role=?", [role])
+        self.cursor.execute("select * from Raw_materials where  role=?", [role])
         raw_material_list = [Raw_material(*raw_material) for raw_material in self.cursor.fetchall()]
         self.disconnect()
         return raw_material_list
 
     def find_by_id(self, id):
         self.connect()
-        self.cursor.execute("select * from raw_materials where  id=?", [id])
+        self.cursor.execute("select * from Raw_materials where  id=?", [id])
         raw_material_list = [Raw_material(*raw_material) for raw_material in self.cursor.fetchall()]
         self.disconnect()
         return raw_material_list
 
     def find_by_username_and_password(self, username, password):
         self.connect()
-        self.cursor.execute("select * from raw_materials where username=? and password=?", [username, password])
+        self.cursor.execute("select * from Raw_materials where username=? and password=?", [username, password])
         raw_material_list = [Raw_material(*raw_material) for raw_material in self.cursor.fetchall()]
         self.disconnect()
         return raw_material_list
 
     def find_by_salary_min(self, salary):
         self.connect()
-        self.cursor.execute("select * from raw_materials where salary=?", [salary])
+        self.cursor.execute("select * from Raw_materials where salary=?", [salary])
         raw_material_list = [Raw_material(*raw_material) for raw_material in self.cursor.fetchall()]
         self.disconnect()
         return raw_material_list
@@ -98,7 +97,7 @@ class Raw_material_repository:
     def reset_username_password(self, raw_material):
         self.connect()
         self.cursor.execute(
-            "update raw_materials set username=?,password=?) where id=?",
+            "update Raw_materials set username=?,password=?) where id=?",
             [raw_material.username, raw_material.password, raw_material.id])
 
         self.connection.commit()
@@ -107,7 +106,7 @@ class Raw_material_repository:
 
     def get_all(self):
         self.connect()
-        self.cursor.execute("select * from raw_materials")
+        self.cursor.execute("select * from Raw_materials")
         raw_material_list = [Raw_material(*raw_material) for raw_material in self.cursor.fetchall()]
         self.disconnect()
         return raw_material_list
